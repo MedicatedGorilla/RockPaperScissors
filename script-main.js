ded438f7558ef40b8c5765d47c2986ae4971586b
Jake <69864097+MedicatedGorilla@users.noreply.github.com>
2023-10-26 14:56:23 -0700

    Added a UI and buttons via javascript and DOM manipulation. Variables are global delcared but switching tointerating through a live nodelist after queryselectorall would probably be better.

--- script-main.js
+++ script-main.js
@@ -1,3 +1,51 @@
+let startButton = document.getElementById('startButton');
+let rockButton = document.createElement('button');
+let paperButton = document.createElement('button');
+let scissorButton = document.createElement('button');
+let playerScore = document.createElement('div');
+let computerScore = document.createElement('div');
+let playerScoreCount = 0;
+let computerScoreCount = 0;
+
+startButton.addEventListener('click', () => {
+    rockButton.setAttribute('id', 'rock');
+    paperButton.setAttribute('id','paper');
+    scissorButton.setAttribute('id','scissior');
+    playerScore.setAttribute('id','score');
+    computerScore.setAttribute('id','score');
+
+    rockButton.innerText = "Rock";
+    paperButton.innerText = 'Paper';
+    scissorButton.innerText = 'Scissor';
+    playerScore.innerText = "Player Score: 0";
+    computerScore.innerText = "Computer Score: 0";
+
+    startButton.remove();
+    document.body.append(playerScore);
+    document.body.append(computerScore);
+    document.body.appendChild(scissorButton);
+    document.body.appendChild(paperButton);
+    document.body.appendChild(rockButton);
+});
+
+rockButton.addEventListener('click', () =>{
+    playGame(1);
+})
+paperButton.addEventListener('click', () =>{
+    playGame(2);
+})
+scissorButton.addEventListener('click', () =>{
+    playGame(3);
+})
+
+function endGame(name){
+    computerScore.remove();
+    scissorButton.remove();
+    paperButton.remove();
+    rockButton.remove();
+    playerScore.innerText = name + ' wins!';
+}
+
 function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
@@ -6,56 +54,89 @@ function getRandomInt(min, max) {
 
 
 
-function getPlayerSelection(){
-    let answer = parseInt(prompt("Pick an option: 1. Rock 2. Paper 3. Scissors"))
-    return answer
-}
-
-function playGame(){
-    let points = 0;
+function playGame(player){
     const rps = ["Rock", "Paper", "Scissors"]
     let computer = getRandomInt(1,3)
-    let player = getPlayerSelection()
     if( player == computer){
         console.log("The match is a tie!")
         
     } else if(player == 1 && computer == 3){
         console.log("You win the match!")
-        points += 1;
+        playerScoreCount = playerScoreCount + 1;
     } else if(player == 2 && computer ==1){
         console.log("You win the match!")
-        points += 1;
+        playerScoreCount = playerScoreCount + 1;
     } else if(player == 3 && computer == 2){
-        points += 1;
+        playerScoreCount = playerScoreCount + 1;
         console.log("You win the match!")
     } else {
         console.log("You lose the match!")
-        points -= 1
+        computerScoreCount = computerScoreCount + 1;
     }
+    playerScore.innerText = 'Player Score: '+ playerScoreCount;
+    computerScore.innerText = 'Computer Score: ' + computerScoreCount;
     console.log("Your choice: " + rps[player-1] + ", Computer choice: " + rps[computer-1])
-    return points
+
+    if(playerScoreCount == 5){
+        endGame('You')
+    } else if(computerScoreCount == 5){
+        endGame('Computer');
+    }
 }
 
-function playRound(){
-    let points = 0;
-    for(let i = 1; i <= 5; i++){
-        console.log("Round: " + i)
-        points = points + playGame()
-        if(i >= 3){
-            if(points - (5-i)<= 0){
-                console.log("You lost the game :(")
-                break;
-            } else if(points - (5-i)>=0){
-                console.log("You won the game!")
-                break;
-            }
-        }
-    }
-    if (points < 0) {
-        console.log("You lost the game :(");
-    } else if (points > 0) {
-        console.log("You won the game!")
-    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function playRound(){
+//     let points = 0;
+//     for(let i = 1; i <= 5; i++){
+//         console.log("Round: " + i)
+//         points = points + playGame()
+//         if(i >= 3){
+//             if(points - (5-i)<= 0){
+//                 console.log("You lost the game :(")
+//                 break;
+//             } else if(points - (5-i)>=0){
+//                 console.log("You won the game!")
+//                 break;
+//             }
+//         }
+//     }
+//     if (points < 0) {
+//         console.log("You lost the game :(");
+//     } else if (points > 0) {
+//         console.log("You won the game!")
+//     }
     
     
-}
+//}
